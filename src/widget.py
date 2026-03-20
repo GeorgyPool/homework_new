4424a4f9c6966308d123e634b77cab4422c2a338
Georgy <girt80@yandex.ru>
2026-03-20 12:17:24 +0300

    add func mask_account_carf in module widget.py

---
+++ src/widget.py
@@ -0,0 +1,21 @@
+from masks import get_mask_account, get_mask_card_number  # type: ignore
+
+
+def mask_account_card(bank_info: str) -> str:
+    """Возвращает строку с замаскированным номером для карт и счетов"""
+    new_number_str = ""
+    new_words_list = []
+
+    split_bank_info = bank_info.split()
+    for index in split_bank_info:
+        if index.isalpha():
+            new_words_list.append(index)
+        else:
+            new_number_str += index
+
+    if len(new_number_str) > 16:
+        bank_account = get_mask_account(new_number_str)
+        return f"{" ".join(new_words_list)} {bank_account}"
+    else:
+        bank_card = get_mask_card_number(new_number_str)
+        return f"{" ".join(new_words_list)} {bank_card}"
