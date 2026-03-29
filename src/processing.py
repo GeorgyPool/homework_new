1cff1b2c22ea11bf6a23fe3e1f668c0c4e9772a6
Georgy <girt80@yandex.ru>
2026-03-29 11:07:21 +0300

    refinement of type annotations

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
-def filter_by_state(list_of_dict, state_key="EXECUTED") -> list[dict]:
-    """Функция возвращает список словарей только тех где ключ равен 'state_key' """
+def filter_by_state(list_of_dict: list[dict], state_key: str = "EXECUTED") -> list[dict]:
+    """Сортирует список словарей по ключу"""
     new_list_dict = []
 
     for index in list_of_dict:
@@ -22,8 +22,8 @@ print(
 )
 
 
-def sort_by_date(list_of_dict, key_data=True) -> list[dict]:
-    """Функция возвращающая отсортированый список словарей по дате по убыванию по умалчанию"""
+def sort_by_date(list_of_dict: list[dict], key_data: bool = True) -> list[dict]:
+    """Сортирует список словарей по дате (по убыванию)"""
     if key_data:
         sorted_list = sorted(list_of_dict, key=lambda x: x["date"], reverse=True)
         return sorted_list
