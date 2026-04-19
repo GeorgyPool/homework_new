b162842f7039140174dc8bbd5b9c039c106a6fb8
Georgy <girt80@yandex.ru>
2026-04-19 12:04:13 +0300

    complete linters

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 from tests.conftest import corrected_info_for_filter_by_currency
 
 
