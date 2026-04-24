86ee2f716a6b5e6d5807182713a156047a046884
Georgy <girt80@yandex.ru>
2026-04-24 17:10:55 +0300

    linters for decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,11 +1,18 @@
 def log(filename=None):
+    """Декоратор записывающий лог в файл если передан
+    аргумент filename, если аргумент не предан
+    то выводит лог в консоль"""
+
     def log_inf(func):
         def wrapper(*args, **kwargs):
+            # Если во время вызова основной функции возникает ошибка
+            # То информация об ошибке записывается в лог
             try:
                 result = func(*args, **kwargs)
                 if isinstance(filename, str):
                     with open(filename, "w") as file:
                         file.write(f"{func.__name__} ok")
+                        return f"записано в лог файла {filename}"
                 else:
                     return f"{func.__name__} ok, результат:{result}"
             except Exception as error:
@@ -26,4 +33,4 @@ def my_func(x, y):
     return x + y
 
 
-print(my_func(5, 9))
+print(my_func(1, 2))
