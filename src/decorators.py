8bc9c3b96f2f3ac41c1506b38b39d9c1ea41e7ac
Georgy <girt80@yandex.ru>
2026-04-24 10:38:40 +0300

    add module decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+def log(filename=None):
+    def log_inf(func):
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args)
+                if isinstance(filename, str):
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} ok")
+                else:
+                    return f"{func.__name__} ok, результат:{result}"
+            except Exception as error:
+                error_mes = f"{func.__name__} error: {type(error).__name__}. Inputs: {args}, {kwargs}"
+                if isinstance(filename, str):
+                    with open(filename, "w") as file:
+                        file.write(error_mes)
+                else:
+                    return error_mes
+
+        return wrapper
+
+    return log_inf
+
+
+@log("mylog.txt")
+def my_func(x, y):
+    return x + y
+
+
+print(my_func(5, ))
