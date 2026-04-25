a3f65d6ec10081828c9c8bc1e48905c920fafa15
Georgy <girt80@yandex.ru>
2026-04-25 09:38:16 +0300

    add test_decorators.py

---
+++ tests/test_decorators.py
@@ -0,0 +1,39 @@
+from src.decorators import log
+
+#Проверка, что при отсутствии аргумента в декораторе, вывод выводится в консоль
+def test_correct_in_consol(capsys):
+    @log()
+    def addition_numbers(x, y):
+        return x + y
+    result = addition_numbers(1, 2)
+    print(result)
+    captured = capsys.readouterr()
+    assert captured.out == "addition_numbers ok, результат:3\n"
+
+
+#Проверка, что при отсутствии аргумента в декораторе, вывод выводится в консоль
+def test_correct_invalid_in_consol(capsys):
+    @log()
+    def addition_numbers(x, y):
+        return x + y
+    result = addition_numbers(1)
+    print(result)
+    captured = capsys.readouterr()
+    assert captured.out == "addition_numbers error: TypeError. Inputs: (1,), {}\n"
+
+
+#Проверяет что при передаче аргумента в декораторе создается файл с записью о работе функции
+def test_correct_write_in_file(tmp_path):
+    file = tmp_path / "test_write.txt"
+    @log(str(file))
+    def addition_numbers(x, y):
+        return x + y
+
+    result = addition_numbers(1, 2)
+
+    assert file.is_file()
+
+    with open(file, "r") as text:
+        content = text.read()
+
+    assert content == "addition_numbers ok, результат: 3"
