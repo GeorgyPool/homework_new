a7dd2d8d07060f6ca80881987c65dfb6dbb9224f
Georgy <girt80@yandex.ru>
2026-05-03 12:10:57 +0300

    add src/module external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,51 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+def convert_currency(transactions: dict) -> float:
+    """Конвертирует валюту в рубли если она в USD EUR и т.д
+    Если валюта в рублях то выводит сумму 'amount'"""
+
+    if transactions["operationAmount"]["currency"]["code"] == "RUB":
+        return float(transactions["operationAmount"]["amount"])
+
+    else:
+        load_dotenv()
+        payload = {}
+        head = {"apikey": os.getenv("API_KEY")}
+
+        first_c = transactions["operationAmount"]["currency"]["code"]
+        amount_c = transactions["operationAmount"]["amount"]
+
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={first_c}&amount={amount_c}"
+        response = requests.request("GET", url, headers=head, data=payload)
+        return float(response.json()["result"])
+
+
+if __name__ == "__main__":
+    a = convert_currency(
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560",
+        }
+    )
+    b = convert_currency(
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
+        }
+    )
+    print(a)
+    print(b)
