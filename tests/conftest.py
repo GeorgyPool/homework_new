bd6895a4f46f5f02f496fe7a764d322922ba443a
Georgy <girt80@yandex.ru>
2026-05-04 09:46:47 +0300

    add tests/test_external_api.py, .env.example

--- tests/conftest.py
+++ tests/conftest.py
@@ -224,3 +224,19 @@ def dot_have_key_in_list():
             "to": "Счет 14211924144426031657",
         },
     ]
+
+#фикстуры для test_external_api.py
+@pytest.fixture
+def currency_rub():
+    return {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    }
+    }
---
+++ tests/test_external_api.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+from src.external_api import convert_currency
+
+#тест функция открывает request.get для запроса конвертации курса валют
+@patch("requests.request")
+def test_api_convert_currency(mock_get):
+    mock_get.return_value.json.return_value = {"result": 1}
+    assert convert_currency({"operationAmount": {"amount": 1, "currency": {"code": "USD"}}}) == 1
+    mock_get.assert_called_once()
+
+#тест функции на корректную отдачу суммы если она указана в рублях
+def test_currency_rub(currency_rub):
+    assert convert_currency(currency_rub) == 31957.58
